b7333c24219b4611969256558627e74f6459dea4
chucky07 <endrych.d@seznam.cz>
2017-11-13 13:34:41 +0100

    Update and rename draw.py to main.py

--- open_window/main.py
+++ open_window/main.py
@@ -2,10 +2,16 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+class Handler:
+    def btn_click_off(self, *args):
+        Gtk.main_quit(*args)
 
+    def btn_click_msg(self, button):
+        print("Hello World!")
 
 builder = Gtk.Builder()
 builder.add_from_file("okno.glade")
+builder.connect_signals(Handler())
 
 window = builder.get_object("dialog1")
 window.show_all()
